4331b4672e108d9bd76b6ac525a2c3f80f3383f8
AlexanderCWM <petrovaleksandr9997@gmail.com>
2018-10-17 23:14:40 +0300

    page without adaptive

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.about_us_section_carousel').slick({
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear',
+    // appendArrows: '.slider_arrows',
+    prevArrow: '<a class="carousel_arrow slick-prev"><i class="fas fa-chevron-left"></i></a>',
+    nextArrow: '<a class="carousel_arrow slick-next"><i class="fas fa-chevron-right"></i></a>',
+  });
+      
